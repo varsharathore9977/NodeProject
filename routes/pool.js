2c8cd9378f26057906d76a4af79bdbd790ca5c35
varsharathore9977 <59653377+varsharathore9977@users.noreply.github.com>
2022-07-02 09:00:43 +0530

    Update pool.js

--- routes/pool.js
+++ routes/pool.js
@@ -3,7 +3,7 @@ var pool=mysql.createPool(
 {    host:'localhost',
 port:3306,
 user:'root',
-password:'123',
+password:'xxx',
 database:'electronicsproduct',
 connectionLimit:100,
 multipleStatement:true
